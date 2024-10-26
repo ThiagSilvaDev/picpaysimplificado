9f4f48a6c38cb71caf65ac6731835144ec4f54d2
ThiagSilvaDev <thiagsilvadev@gmail.com>
2024-10-26 01:33:39 -0300

    feat: transaction management, validation and data response

--- src/main/java/com/thiagsilvadev/picpaysimplificado/domain/transaction/Transaction.java
+++ src/main/java/com/thiagsilvadev/picpaysimplificado/domain/transaction/Transaction.java
@@ -15,6 +15,7 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity(name ="transactions")
@@ -23,6 +24,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
+@NoArgsConstructor
 public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
